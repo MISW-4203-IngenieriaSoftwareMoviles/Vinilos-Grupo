81ad0037992f89448cb7650e38e69249c4ee9b81
Jenny Idarraga <j.idarragar@uniandes.edu.co>
2022-11-03 12:40:27 -0500

    HU01 Creación Repositories

--- app/src/main/java/com/example/vinilos/viewmodels/AlbumViewModel.kt
+++ app/src/main/java/com/example/vinilos/viewmodels/AlbumViewModel.kt
@@ -4,9 +4,12 @@ import android.app.Application
 import androidx.lifecycle.*
 import com.example.vinilos.models.Album
 import com.example.vinilos.network.NetworkServiceAdapter
+import com.example.vinilos.repositories.AlbumRepository
+
 
 class AlbumViewModel(application: Application) : AndroidViewModel(application) {
 
+    private val albumsRepository = AlbumRepository(application)
     private val _albums = MutableLiveData<List<Album>>()
 
     val albums: LiveData<List<Album>>
@@ -27,11 +30,11 @@ class AlbumViewModel(application: Application) : AndroidViewModel(application) {
     }
 
     private fun refreshDataFromNetwork() {
-        NetworkServiceAdapter.getInstance(getApplication()).getAlbums({
+        albumsRepository.refreshData({
             _albums.postValue(it)
             _eventNetworkError.value = false
             _isNetworkErrorShown.value = false
-        }, {
+        },{
             _eventNetworkError.value = true
         })
     }
