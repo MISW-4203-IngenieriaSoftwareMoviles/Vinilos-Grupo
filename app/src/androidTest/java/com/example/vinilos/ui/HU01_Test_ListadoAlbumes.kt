e01d4bdaad73665d2916a30ea9bdb88d5af39610
SaragRam <sg.ramirez940@uniandes.edu.co>
2022-11-06 18:11:05 -0500

    Feature/hu01 pruebas (#52)
    * Create readme.md
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Adición mockups artista
    
    * Delete listarColeccionistas.JPG
    
    * Delete mockupDetalleColeccionista.JPG
    
    * Add files via upload
    
    * Delete mockupDetalleColeccionista.JPG
    
    * Add files via upload
    
    * Delete mockupDetalleColeccionista.JPG
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Update : Se agregan pruebas HU01
    
    * Update: ajuste prueba título álbum
    
    Co-authored-by: gcastrot82 <g.castrot@uniandes.edu.co>
    Co-authored-by: jidarragar <j.idarragar@uniandes.edu.co>
    Co-authored-by: Gerardo Castro Tamayo <gcastrot82@hotmail.com>

---
+++ app/src/androidTest/java/com/example/vinilos/ui/HU01_Test_ListadoAlbumes.kt
@@ -0,0 +1,73 @@
+import android.view.View
+import android.view.ViewGroup
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.*
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import com.example.vinilos.R
+import com.example.vinilos.ui.MainActivity
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.`is`
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.TypeSafeMatcher
+import org.hamcrest.core.IsInstanceOf
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest4 {
+
+    @Rule
+    @JvmField
+    var mActivityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun mainActivityTest4() {
+        val materialButton = onView(
+            allOf(
+                withId(R.id.btn_visitante), withText("SOY VISITANTE"),
+                childAtPosition(
+                    childAtPosition(
+                        withClassName(`is`("android.widget.FrameLayout")),
+                        0
+                    ),
+                    2
+                ),
+                isDisplayed()
+            )
+        )
+        materialButton.perform(click())
+        Thread.sleep(3_000)
+        val textView = onView(
+            allOf(
+                withId(R.id.album_name), withText("Poeta del pueblo"),
+                withParent(withParent(IsInstanceOf.instanceOf(androidx.cardview.widget.CardView::class.java))),
+                isDisplayed()
+            )
+        )
+        textView.check(matches(withText("Poeta del pueblo")))
+    }
+
+    private fun childAtPosition(
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+}
