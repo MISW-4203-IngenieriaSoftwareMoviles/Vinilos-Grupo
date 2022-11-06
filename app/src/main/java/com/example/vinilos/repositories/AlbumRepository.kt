937897e61b5b04336910f7f4644b5a47b02b3304
gcastrot82 <g.castrot@uniandes.edu.co>
2022-11-05 20:51:34 -0500

    Update: Ajuste adaptador para detalle album (#50)

--- app/src/main/java/com/example/vinilos/repositories/AlbumRepository.kt
+++ app/src/main/java/com/example/vinilos/repositories/AlbumRepository.kt
@@ -17,14 +17,14 @@ class AlbumRepository(val application: Application) {
         )
     }
 
-    fun refreshDataDetailAlbum(callback: (Album) -> Unit, onError: (VolleyError) -> Unit, id: Int) {
+    fun refreshDataDetailAlbum(id: Int, callback: (Album) -> Unit, onError: (VolleyError) -> Unit) {
         //Determinar la fuente de datos que se va a utilizar. Si es necesario consultar la red, ejecutar el siguiente código
-        NetworkServiceAdapter.getInstance(application).getAlbum(
+        NetworkServiceAdapter.getInstance(application).getAlbum(id,
             {
                 //Guardar los albumes de la variable it en un almacén de datos local para uso futuro
                 callback(it)
             },
-            onError, id
+            onError
         )
     }
 }
