d471685ce72abfe13bde2bcd1bcbda54432e5cd4
jidarragar <j.idarragar@uniandes.edu.co>
2022-11-11 15:12:30 -0500

    HU03 creación viewModel, adptadores y fragmentos (#63)

---
+++ app/src/main/java/com/example/vinilos/viewmodels/PerformerViewModel.kt
@@ -0,0 +1,54 @@
+package com.example.vinilos.viewmodels
+
+import android.app.Application
+import androidx.lifecycle.*
+import com.example.vinilos.models.Performer
+import com.example.vinilos.repositories.PerformerRepository
+
+class PerformerViewModel (application: Application) : AndroidViewModel(application) {
+
+    private val performersRepository = PerformerRepository(application)
+
+    private val _performers = MutableLiveData<List<Performer>>()
+
+    val performers: LiveData<List<Performer>>
+        get() = _performers
+
+    private var _eventNetworkError = MutableLiveData<Boolean>(false)
+
+    val eventNetworkError: LiveData<Boolean>
+        get() = _eventNetworkError
+
+    private var _isNetworkErrorShown = MutableLiveData<Boolean>(false)
+
+    val isNetworkErrorShown: LiveData<Boolean>
+        get() = _isNetworkErrorShown
+
+    init {
+        refreshDataFromNetwork()
+    }
+
+    private fun refreshDataFromNetwork() {
+        performersRepository.refreshDataPerformers({
+            _performers.postValue(it)
+            _eventNetworkError.value = false
+            _isNetworkErrorShown.value = false
+        },{
+            _eventNetworkError.value = true
+        })
+    }
+
+    fun onNetworkErrorShown() {
+        _isNetworkErrorShown.value = true
+    }
+
+    class Factory(val app: Application) : ViewModelProvider.Factory {
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(CollectorViewModel::class.java)) {
+                @Suppress("UNCHECKED_CAST")
+                return PerformerViewModel(app) as T
+            }
+            throw IllegalArgumentException("Unable to construct viewmodel")
+        }
+    }
+}
