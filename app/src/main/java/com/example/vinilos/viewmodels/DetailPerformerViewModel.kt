d9bede3b89753efaba038ecd0669d7df78f4b2ec
SaragRam <sg.ramirez940@uniandes.edu.co>
2022-11-12 17:27:47 -0500

    HU04 Update: creación viewmodel

--- app/src/main/java/com/example/vinilos/viewmodels/DetailPerformerViewModel.kt
+++ app/src/main/java/com/example/vinilos/viewmodels/DetailPerformerViewModel.kt
@@ -6,5 +6,52 @@ import com.example.vinilos.models.Performer
 import com.example.vinilos.network.NetworkServiceAdapter
 import com.example.vinilos.repositories.PerformerRepository
 
-class DetailPerformerViewModel {
+class DetailPerformerViewModel (application: Application, id: Int) : AndroidViewModel(application){
+
+    private val performerRepository = PerformerRepository(application)
+
+    private val _performer = MutableLiveData<Performer>()
+
+    val performer: LiveData<Performer>
+        get() = _performer
+
+    private var _eventNetworkError = MutableLiveData<Boolean>(false)
+
+    val eventNetworkError: LiveData<Boolean>
+        get() = _eventNetworkError
+
+    private var _isNetworkErrorShown = MutableLiveData<Boolean>(false)
+
+    val isNetworkErrorShown: LiveData<Boolean>
+        get() = _isNetworkErrorShown
+
+    val id: Int = id
+
+    init {
+        refreshDataFromNetwork()
+    }
+
+    private fun refreshDataFromNetwork() {
+        performerRepository.refreshDataDetailPerformer(id, {
+            _performer.postValue(it)
+            _eventNetworkError.value = false
+            _isNetworkErrorShown.value = false
+        }, {
+            _eventNetworkError.value = true
+        })
+    }
+
+    fun onNetworkErrorShown() {
+        _isNetworkErrorShown.value = true
+    }
+
+    class Factory(val app: Application, val id: Int) : ViewModelProvider.Factory {
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(DetailPerformerViewModel::class.java)) {
+                @Suppress("UNCHECKED_CAST")
+                return DetailPerformerViewModel(app, id) as T
+            }
+            throw IllegalArgumentException("Unable to construct viewmodel")
+        }
+    }
 }
