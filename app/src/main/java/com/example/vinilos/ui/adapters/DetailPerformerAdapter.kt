8aa2ce164f30a6aaa66ba0d16281137fb8cee384
SaragRam <sg.ramirez940@uniandes.edu.co>
2022-11-12 18:48:43 -0500

    HU04 Update: creación fragmento, adaptador, navegación

--- app/src/main/java/com/example/vinilos/ui/adapters/DetailPerformerAdapter.kt
+++ app/src/main/java/com/example/vinilos/ui/adapters/DetailPerformerAdapter.kt
@@ -9,8 +9,42 @@ import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.example.vinilos.models.Performer
 import com.example.vinilos.R
-//import com.example.vinilos.databinding.DetailAlbumFragmentBinding
-//import com.example.vinilos.ui.AlbumFragmentDirections
+import com.example.vinilos.databinding.DetailPerformerFragmentBinding
+import com.example.vinilos.ui.PerformerFragmentDirections
 
-class DetailPerformerAdapter {
+class DetailPerformerAdapter: RecyclerView.Adapter<DetailPerformerAdapter.DetailPerformerViewHolder>() {
+
+    var peformer : Performer? = null
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DetailPerformerViewHolder {
+        val withDataBinding: DetailPerformerFragmentBinding = DataBindingUtil.inflate(
+            LayoutInflater.from(parent.context),
+            DetailPerformerViewHolder.LAYOUT,
+            parent,
+            false)
+        return DetailPerformerViewHolder(withDataBinding)
+    }
+
+    override fun onBindViewHolder(holder: DetailPerformerViewHolder, position: Int) {
+        holder.viewDataBinding.also {
+            it.performer = peformer
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return 1
+    }
+
+
+    class DetailPerformerViewHolder(val viewDataBinding: DetailPerformerFragmentBinding) :
+        RecyclerView.ViewHolder(viewDataBinding.root) {
+        companion object {
+            @LayoutRes
+            val LAYOUT = R.layout.detail_performer_fragment
+        }
+    }
 }
