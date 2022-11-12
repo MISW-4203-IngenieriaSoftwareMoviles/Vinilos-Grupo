cc301e2fa38e5bc78e6e59b76a654c2c900870bb
SaragRam <sg.ramirez940@uniandes.edu.co>
2022-11-12 17:17:20 -0500

    HU04 Update: consumo API (#65)

---
+++ app/src/main/java/com/example/vinilos/ui/adapters/DetailPerformerAdapter.kt
@@ -0,0 +1,16 @@
+package com.example.vinilos.ui.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.RecyclerView
+import com.example.vinilos.models.Performer
+import com.example.vinilos.R
+//import com.example.vinilos.databinding.DetailAlbumFragmentBinding
+//import com.example.vinilos.ui.AlbumFragmentDirections
+
+class DetailPerformerAdapter {
+}
